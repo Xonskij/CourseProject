743db01262378a5d0978a4eef6e79f3d24d6e649
fgqfgqfgq <fgqfgqfgq654123@gmail.com>
2016-02-14 12:34:54 +0700

    Zakommitil epta 222

--- app/src/main/java/com/example/user/courseproject/dxds.java
+++
@@ -1,8 +0,0 @@
-package com.example.user.courseproject;
-
-/**
- * Created by user on 14.02.2016.
- */
-public class dxds {
-}
-
--- app/src/main/java/com/example/user/courseproject/et.java
+++
@@ -1,9 +0,0 @@
-package com.example.user.courseproject;
-
-/**
- * Created by user on 13.02.2016.
- */
-public class et {
-    //asfghsd;
-}
-//rtt
