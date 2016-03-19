c513fbe6bf07ddacb3a64c130650a970c5ddc739
PapkoPA <trait95@yandex.ru>
2016-03-19 20:30:48 +0300

    Добавил меню со списком ФПС

---
+++ app/src/main/java/com/example/user/courseproject/SelectFpsClass.java
@@ -0,0 +1,57 @@
+package com.example.user.courseproject;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.media.CamcorderProfile;
+import android.os.Bundle;
+import android.widget.Toast;
+
+/**
+ * Created by Павел on 19.03.2016.
+ */
+public class SelectFpsClass extends DialogFragment {
+    String selection;
+    public static Integer size = CamcorderProfile.QUALITY_TIME_LAPSE_1080P;
+    final CharSequence[] items = {"1 second","5 second","10 second","15second","30 second"};
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Frame Interval").setSingleChoiceItems(items, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                switch (arg1) {
+                    case 0:
+                        selection = (String) items[arg1];
+                        //size = CamcorderProfile.QUALITY_TIME_LAPSE_1080P;
+                        break;
+                    case 1:
+                        selection = (String) items[arg1];
+                        //  size = CamcorderProfile.QUALITY_TIME_LAPSE_720P;
+                        break;
+                    case 2:
+                        selection = (String) items[arg1];
+                        //  size = CamcorderProfile.QUALITY_TIME_LAPSE_480P;
+                        break;
+                    case 3:
+                        selection = (String) items[arg1];
+                        //  size = CamcorderProfile.QUALITY_TIME_LAPSE_QVGA;
+                        break;
+                    case 4:
+                        selection = (String) items[arg1];
+                        //  size = CamcorderProfile.QUALITY_TIME_LAPSE_QVGA;
+                        break;
+                }
+            }
+        }).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getActivity(), "You choose frame interval: " + selection, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return builder.create();
+    }
+}
